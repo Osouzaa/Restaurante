1106a41c15cd8c1051067272687e403aafc968b3
GabrielsouzaDev <101714272+Osouzaa@users.noreply.github.com>
2023-03-10 12:39:41 -0300

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import Cardapio from 'pages/Cardapio';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'normalize.css'
+import './index.css';
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Cardapio />
+  </React.StrictMode>
+);
+
+
